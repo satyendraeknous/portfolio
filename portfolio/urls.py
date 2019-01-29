6037cdcb0b1e3b92bdf66ac643571e3a1378cb51
Ankit766254 <ankit@eknous.com>
2019-01-30 02:07:01 +0530

    second commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+import jobs.views
+
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', jobs.views.home, name='home'),
+
+
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
